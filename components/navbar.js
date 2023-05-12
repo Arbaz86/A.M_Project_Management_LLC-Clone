b9576c12c6ab68e28b7df5720748a5cbbc16ab3f
Arbaz Hussain <arbazh777@gmail.com>
2023-05-12 22:33:43 +0530

    Added favicon, improved code readability with comments in index.js, created footer layout, and added products section with CSS styling to index.html.

--- components/navbar.js
+++ components/navbar.js
@@ -1,73 +1,77 @@
 const topNavbar = `
-    <div class="site-wrap">
-        <div class="top-bar">
-            <div class="top-bar-left-social">
-                <a href="#">
-                <i class="fa fa-facebook-f"></i>
-                </a>
-                <a href="#">
-                <i class="fa fa-twitter"></i>
-                </a>
-                <a href="#">
-                <i class="fa fa-linkedin"></i>
-                </a>
-                <a href="#">
-                <i class="fa fa-google-plus"></i>
-                </a>
-            </div>
-            <div class="top-bar-right-time">
-                <i class="fa fa-clock-o"></i>
-                Mon – Sat: 9:00 – 19:00
-            </div>
-            </div>
-        </div>
-
-        <div class="site-header">
-            <div class="header-logo">
-            <img
-                src="https://ampm-llc.com/wp-content/uploads/2022/11/Thank-you-for-comingvvv.png"
-                alt="logo"
-            />
-            </div>
-            <div class="header-contact-em-ph">
-            <div class="header-contact-details">
-                <div class="contact-icon-email">
-                <i
-                    class="fa fas fa-envelope-open"
-                    style="color: #016be1; font-size: 24px"
-                ></i>
-                </div>
-                <div class="contact-email-details">
-                <div>Email Us</div>
-                <div>info@ampm-llc.com</div>
-                </div>
-            </div>
-            <div class="header-contact-details">
-                <div class="contact-icon-phone">
-                <i
-                    class="fa fas fa-mobile"
-                    style="color: #016be1; font-size: 24px"
-                ></i>
-                </div>
-                <div class="contact-phone-details">
-                <div>Call Us</div>
-                <div>+971 54 445 3622</div>
-                </div>
-            </div>
-            </div>
-        </div>
-        <nav class="navbar-sticky">
-            <div class="nav-links">
-            <a href="/index.html">HOME</a>
-            <a href="/">ABOUT US </a>
-            <a href="/">PRODUCTS</a>
-            <a href="/">WHY US</a>
-            <a href="/">PARTNERS & ASSOCIATES</a>
-            <a href="/">CONTACT US</a>
-            <button class="nav-download-btn">DOWNLOAD COMPANY PROFILE</button>
-            </div>
-        </nav>
+<!-- This code represents the top section of a website -->
+<div class="site-wrap">
+  <!-- This div contains the top bar with social media links and opening hours -->
+  <div class="top-bar">
+    <div class="top-bar-left-social">
+      <a href="#">
+        <i class="fa fa-facebook-f"></i>
+      </a>
+      <a href="#">
+        <i class="fa fa-twitter"></i>
+      </a>
+      <a href="#">
+        <i class="fa fa-linkedin"></i>
+      </a>
+      <a href="#">
+        <i class="fa fa-google-plus"></i>
+      </a>
     </div>
+    <div class="top-bar-right-time">
+      <i class="fa fa-clock-o"></i>
+      Mon – Sat: 9:00 – 19:00
+    </div>
+  </div>
+</div>
+<!-- This div contains the site header with logo, contact details, and navigation menu -->
+<div class="site-header">
+  <!-- Logo -->
+  <div class="header-logo">
+    <img
+      src="https://ampm-llc.com/wp-content/uploads/2022/11/Thank-you-for-comingvvv.png"
+      alt="logo"
+    />
+  </div>
+  <div class="header-contact-em-ph">
+    <!-- Contact details -->
+    <div class="header-contact-details">
+      <div class="contact-icon-email">
+        <i
+          class="fa fas fa-envelope-open"
+          style="color: #016be1; font-size: 24px"
+        ></i>
+      </div>
+      <div class="contact-email-details">
+        <div>Email Us</div>
+        <div>info@ampm-llc.com</div>
+      </div>
+    </div>
+    <div class="header-contact-details">
+      <div class="contact-icon-phone">
+        <i
+          class="fa fas fa-mobile"
+          style="color: #016be1; font-size: 24px"
+        ></i>
+      </div>
+      <div class="contact-phone-details">
+        <div>Call Us</div>
+        <div>+971 54 445 3622</div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Navigation menu -->
+<nav class="navbar-sticky">
+  <div class="nav-links">
+    <a href="/index.html">HOME</a>
+    <a href="/">ABOUT US </a>
+    <a href="/">PRODUCTS</a>
+    <a href="/">WHY US</a>
+    <a href="/">PARTNERS & ASSOCIATES</a>
+    <a href="/">CONTACT US</a>
+    <button class="nav-download-btn">DOWNLOAD COMPANY PROFILE</button>
+  </div>
+</nav>
 `;
 
 document.querySelector(".top-nav-bar").innerHTML = topNavbar;
--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,50 @@
+// This variable keeps track of the current slide index
 let slideIndex = 1;
+
+// Call the showSlides function to display the first slide
 showSlides(slideIndex);
 
+// This function moves to the next or previous slide
 function plusSlides(n) {
   showSlides((slideIndex += n));
 }
 
+// This function displays a specific slide
 function currentSlide(n) {
   showSlides((slideIndex = n));
 }
 
+// This function shows the slide at the current slideIndex and hides the others
 function showSlides(n) {
-  let i;
+  // Get all the slides and dots elements
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
+
+  // If slideIndex is greater than the number of slides, reset it to 1
   if (n > slides.length) {
     slideIndex = 1;
   }
+  // If slideIndex is less than 1, set it to the number of slides
   if (n < 1) {
     slideIndex = slides.length;
   }
-  for (i = 0; i < slides.length; i++) {
+
+  // Hide all the slides
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
+
+  // Remove the 'active' class from all the dots
+  for (let i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
+
+  // Show the slide at the current slideIndex and add the 'active' class to the corresponding dot
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
 
+// This interval function calls the plusSlides function every 5 seconds to move to the next slide
 setInterval(() => {
   plusSlides(1);
 }, 5000);
